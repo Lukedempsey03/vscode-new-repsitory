1a9fb0e5d8ef8923ac098792d0e7223147f86c47
Luke <59899366+lukedempsey1@users.noreply.github.com>
2023-01-04 20:18:24 -0800

    Actually Updated File

--- MusicProgC++.cpp
+++ MusicProgC++.cpp
@@ -10,28 +10,15 @@ int keyFn(){
     i = (rand()%7)+1; 
     return i;
 }
-int accentFn(){
+int accentRand(){
    srand((unsigned)time(0)); 
     int i;
     i = (rand()%3)+1; 
     return i;  
 }
-/*int progressionFn(){
-
-     //return ran;
-   
-}*/
-// need to have a one 
-
-int main() { 
-    bool Mmprog;
-    int Key_m = 96 + keyFn();
-    int Key_M = 64 + keyFn();
-    char character_m =char(Key_m);
-    char character_M =char(Key_M);
-
-    string accent; 
-    int accentVar = accentFn();
+string accentFn(){
+     string accent; 
+    int accentVar = accentRand();
     switch (accentVar){
     case 1:
         accent = "Flat";
@@ -46,38 +33,49 @@ int main() {
     default:
         break;
     }
+    return accent;
+} 
+void shuffle(int prog[], int len){
+  int temp =0;
+  int randomIndex =0;
+    for (int i = 0; i < len; i++)
+    {
+        randomIndex = rand() % len;
+       temp = prog[i];
+       prog[i] = prog[randomIndex];
+       prog[randomIndex] = temp;
+
+    }
+    
+}
+int main() { 
+    bool Mmprog;
+    int Key_m = 96 + keyFn();
+    int Key_M = 64 + keyFn();
+    char character_m =char(Key_m);
+    char character_M =char(Key_M);
 
     int i;
     int random1, random2, random3;
     srand((unsigned)time(0)); 
-    
     random1 = (rand()%7)+1;
     random2 = (rand()%7)+1;
     random3 = (rand()%7)+1;
-    int prog[] = {1, random1, random2, random3};
-    int index;
-    //srand(time(0));
-    int temp =0; 
-    /*for (i =0; i , i < 4; i++){
-        index = rand() %4; 
-        temp = prog[];
-        prog[i] = prog[index];
-        prog[index] = temp;
-    }*/
-    
-    //cout << prog[index];
+    int prog[4] = {1, random1, random2, random3};
 
+    shuffle(prog, 4);
 
     cout << "*********************************\n";
     cout << "Welcome to Progression Generator!\n";    
     cout << "Do you want a Major or minor Progression?\n";
     cout << "(For Major enter 0, For minor enter 1)\n";
+    cout << "Enter Here: ";
     cin >> Mmprog; 
 
     if (Mmprog == 0) {
-        cout << "Your Progression is: " << character_M << " " << accent << " Major " << ", And the Progression is: ";
+        cout << "Your Progression is: " << character_M << " " << accentFn() << " Major " << ", And the Progression is: ";
     } else {
-        cout << "Your Progression is: " << character_m << " " << accent << " minor " << ", And the Progression is: ";
+        cout << "Your Progression is: " << character_m << " " << accentFn() << " minor " << ", And the Progression is: ";
     }
 
     int u;
