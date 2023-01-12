4d79f9667d1c5272fe49ecb6ddb0867c3a5cc8ec
Gage Aschenbrenner <gageaschenbrenner@gmail.com>
2023-01-11 21:56:57 -0800

    Update MusicProgC++.cpp

--- MusicProgC++.cpp
+++ MusicProgC++.cpp
@@ -3,9 +3,6 @@
 #include <iostream>
 using namespace std;
 
-// This function uses the same "(rand()%7)+1;"
-// stuff that random1-3 use, try to reuse it so you
-// dont have the same logic scattered around
 int keyFn(){
     srand((unsigned)time(0)); 
     int i;
@@ -64,21 +61,15 @@ int main() {
     char character_m =char(Key_m);
     char character_M =char(Key_M);
 
-    int i;
-    int random1, random2, random3;
     srand((unsigned)time(0));
-    // Try to refactor the below using an array and a for loop
-    // Something like:
-    // for (int i = 0; i < 4; i++) {
-    //  random_nums[i] = (rand()%7)+1;
-    // }
     // Maybe put it in it's own function so it can be reused again as well
-    random1 = (rand()%7)+1;
-    random2 = (rand()%7)+1;
-    random3 = (rand()%7)+1;
-    int prog[4] = {1, random1, random2, random3};
-
-    shuffle(prog, 4);
+    int random_nums[4];
+    random_nums[0] = 1;
+    for (int i = 1; i < 4; i++) {
+        random_nums[i] = (rand()%7)+1;
+    }
+    
+    shuffle(random_nums, 4);
 
     cout << "*********************************\n";
     cout << "Welcome to Progression Generator!\n";    
@@ -97,7 +88,7 @@ int main() {
     int u;
     for(u=0; u<4 ; u++)
     {
-        cout<< prog[u];
+        cout<< random_nums[u];
     }
    
     return 0;  
@@ -106,3 +97,4 @@ int main() {
 // Bonus point stuff:
 // (Easy): Change all the requested changes I typed out above (also by (Easy) I mean relative to the next task not in general)
 // (Hard): Refactor the program to use std::vector instead of arrays
+// (Hard): Make it so that all the numbers in random_nums are unique
